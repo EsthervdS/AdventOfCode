f7f7f97b273d00d1dfd21871383e88c406b1b1eb
Esther <esther.vanderstappen@hu.nl>
2017-12-06 11:27:30 +0100

    Correct solution to day 6. Would be nicer using HashMaps than an ArrayList of Strings.

--- day6/Reallocation.java
+++ day6/Reallocation.java
@@ -3,14 +3,13 @@ import java.util.*;
 
 public class Reallocation {
 
-    private String memoryString;
     private int[] memory;
     private ArrayList<String> configurations;
     private int columns;
 	
     public Reallocation(String fileName) {
 
-	memoryString = IO.readFile(fileName).get(0);
+	String memoryString = IO.readFile(fileName).get(0);
 	String[] blocks = memoryString.split("\t");
 	memory = new int[blocks.length];
 	int i=0;
@@ -26,11 +25,11 @@ public class Reallocation {
     private String configString(int[] config) {
 	String s = "";
 	for (int i=0; i<columns; i++) {
-	    s += Integer.toString(memory[i]);
+	    s += Integer.toString(memory[i]) + "/";
 	}
 	return s;
     }
-    
+
     private int getIndexOfMax() {
 	int ind = 0;
 	int max = -1;
@@ -48,7 +47,11 @@ public class Reallocation {
 	boolean seenBefore = false;
 	for (String s :  configurations) {
 	    seenBefore = (s.equals(configString(memory)));
-	    if (seenBefore) break;
+	    if (seenBefore) {
+		IO.print("Part 1: " + configurations.size());
+		IO.print("Part 2: " + (configurations.size() - configurations.indexOf(s)));
+		break;
+	    }
 	}
 	return seenBefore;
     }
@@ -58,21 +61,17 @@ public class Reallocation {
 	//find column with max #blocks
 	int maxIndex = getIndexOfMax();
 	int distBlocks = memory[maxIndex];
-	
-	//set that column to zero
-	memory[maxIndex] = 0;
-	
-	//iterate over columns and add 1 until all blocks are redistributed
+
+	//iterate over columns and redisitribute blocks
 	for(int i=1; i<=distBlocks; i++) {
 	    memory[(maxIndex+i) % columns]++;
+	    memory[maxIndex]--;
 	}
-	
+
 	//check if configuration has been seen before. If so, repeat. Otherwise: done
 	if (!seenThisConfigBefore()) {
 	    configurations.add(configString(memory));
 	    redistribute();
-	} else {
-	    IO.print("DONE! It took " + configurations.size() + " redistributions.");
 	}
     }
 
