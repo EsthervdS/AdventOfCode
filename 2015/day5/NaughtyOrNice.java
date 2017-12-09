e3969b8a8a049aa5abd7c5b30e9401b803c4b180
Esther <esther.vanderstappen@hu.nl>
2017-12-09 19:34:53 +0100

    2015 Day 5 Part 2

--- 2015/day5/NaughtyOrNice.java
+++ 2015/day5/NaughtyOrNice.java
@@ -9,21 +9,51 @@ public class NaughtyOrNice {
 	lines = IO.readFile(fileName);
     }
 
-    public int howManyAreNice() {
+    public int howManyAreNice1() {
 	int nice = 0;
 	for(int i=0; i<lines.size(); i++) {
 	    boolean vowels = false, dbl = false, allowed = false;
+
 	    vowels = threeVowels(lines.get(i));
 	    dbl = doubleLetters(lines.get(i));
 	    allowed = onlyAllowedSubstrings(lines.get(i));
 	    if (vowels && dbl && allowed) {
 		nice++;
 	    }
-	    
+	}
+	return nice;
+    }
+    public int howManyAreNice2() {
+	int nice = 0;
+	for(int i=0; i<lines.size(); i++) {
+	    boolean dblPair = false, rptLetter = false;
+	    dblPair = doublePair(lines.get(i));
+	    rptLetter = repeatedLetter(lines.get(i));
+	    if (dblPair && rptLetter) {
+		nice++;
+	    }
 	}
 	return nice;
     }
 
+    private boolean doublePair(String str) {
+	boolean d = false;
+	for(int i=0; i<str.length()-1; i++) {
+	    String pair = str.substring(i,i+2);
+	    if (str.substring(i+2).contains(pair)) d = true;
+	}
+	return d;
+    }
+
+    private boolean repeatedLetter(String str) {
+	boolean rpl = false;
+	for(int i=0; i<str.length()-2; i++) {
+	    if (str.charAt(i) == str.charAt(i+2)) rpl = true;
+	}
+	return rpl;
+    }
+
+    
     private boolean threeVowels(String str) {
 	char[] cs = str.toCharArray();
 	int v = 0;
@@ -57,7 +87,8 @@ public class NaughtyOrNice {
     
     public static void main(String[] args) {
 	NaughtyOrNice non = new NaughtyOrNice(args[0]);
-	IO.print("Part 1: " + non.howManyAreNice());
+	IO.print("Part 1: " + non.howManyAreNice1());
+	IO.print("Part 2: " + non.howManyAreNice2());
     }
     
 }
