3544da5c0f14f6f1efeae50bc0cf9350cf692473
Esther <esther.vanderstappen@hu.nl>
2017-12-11 13:32:45 +0100

    Improvement of AoC-template.

--- 2017/Template.java
+++ 2017/Template.java
@@ -9,7 +9,12 @@ public class <name> {
 	lines = IO.readFile(fileName);
     }
 
+    public void process() {
+
+    }
+    
     public static void main(String[] args) {
 	<name> xx = new <name>(args[0]);
+	xx.process();
     }
 }
