48020bc7592f8a369d4e2df822a4b3c4189882a7
Esther <esther.vanderstappen@hu.nl>
2017-12-13 13:27:49 +0100

    Day 13 part 1

---
+++ 2017/day13/Firewall.java
@@ -0,0 +1,54 @@
+import util.*;
+import java.util.*;
+
+public class Firewall {
+
+    ArrayList<String> lines;
+    Scanner[] scanners;
+    int maxDepth;
+    
+    public Firewall(String fileName) {
+	this.lines = IO.readFile(fileName);
+	this.scanners = new Scanner[100];
+	this.maxDepth = 0;
+	
+	for (String l : lines) {
+	    int d = Integer.parseInt(l.split(":")[0].trim());
+	    int r = Integer.parseInt(l.split(":")[1].trim());
+	    scanners[d] = new Scanner(d,r);
+	    if (d>this.maxDepth) this.maxDepth = d;
+	}
+    }
+
+    public int computeSeverity(int delay) {
+	int currTime = delay;
+	int sev = 0;
+
+	int currDepth = 0;
+	while (currDepth <= maxDepth) {
+	    if (scanners[currDepth] != null) {
+		sev += scanners[currDepth].severity(currTime);
+	    }
+	    currDepth++;
+	    currTime++;
+	}
+	return sev;
+    }
+
+    public static void main(String[] args) {
+	Firewall fw = new Firewall(args[0]);
+	
+	int delay=0;
+	while (true) {
+	    IO.print("**** Delay = " + delay + " ****");
+	    int sev = fw.computeSeverity(delay);
+	    if (delay == 0) IO.print("Part 1: " + sev);
+	    if (sev==0) break;
+	    delay++;
+	    
+	}
+	IO.print("Part 2: " + delay);
+	
+    }
+
+}
---
+++ 2017/day13/Scanner.java
@@ -0,0 +1,20 @@
+public class Scanner {
+
+    int depth;
+    int range;
+    
+    public Scanner(int d, int r) {
+	this.depth = d;
+	this.range = r;
+    }
+
+    public int severity(int time) {
+	int sev = 0;
+	if ( time % ((this.range-1) * 2) == 0) {
+	    sev = this.depth * this.range;
+	    System.out.println("Caught at depth : " + this.depth + " at time " + time + " and sev = " + sev);
+	}
+	return sev;
+    }
+
+}
