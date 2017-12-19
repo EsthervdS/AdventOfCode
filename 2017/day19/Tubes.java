ca65efc5639d31382f88e25f56f2adce8ca03b81
Esther <esther.vanderstappen@hu.nl>
2017-12-19 10:33:10 +0100

    Day 19 correct

--- 2017/day19/Tubes.java
+++ 2017/day19/Tubes.java
@@ -15,7 +15,6 @@ public class Tubes {
 	lines = IO.readFile(fileName);
 	nRows = lines.size();
 	nCols = lines.get(0).length();
-	IO.print(nRows + " x " + nCols);
 
 	maze = new char[nRows][nCols];
 	
@@ -30,117 +29,86 @@ public class Tubes {
 	while (maze[currRow][currCol] == ' ') currCol++;
 	currDir = 'd'; // d = down, u = up, l = left, r = right
 	result = "";
-	
-	//IO.print("Start at (" + currRow + ", " + currCol + ")");
-	nSteps = 0;
+	nSteps = 1;
     }
 
 
     private int nextX() {
 	int x=currRow;
-	switch(currDir) {
-	case 'd': {
-	    x += 1;
-	    break;
-	}
-	case 'u': {
-	    x -= 1;
-	    break;
-	}	    
-	default: {
-	    //left or right: x doesn't change
-	    break;
-	}
-	}
+	if (currDir == 'd') x+=1;
+	if (currDir == 'u') x-=1;
 	return x;
     }
 
     private int nextY() {
 	int y=currCol;
-	switch(currDir) {
-	case 'r': {
-	    y += 1;
-	    break;
-	}
-	case 'l': {
-	    y -= 1;
-	    break;
-	}	    
-	default: {
-	    //down or up: y doesn't change
-	    break;
-	}
-	}
+	if (currDir == 'r') y+=1;
+	if (currDir == 'l') y-=1;
 	return y;
     }
 
+    private boolean nextAvailable() {
+	boolean next = false;
+	if ( !(currDir == 'd') && (currRow-1>=0) && (maze[currRow-1][currCol] != ' ') ) next = true;
+	if ( !(currDir == 'u') && (currRow+1<nRows) && (maze[currRow+1][currCol] != ' ') ) next = true;
+	if ( !(currDir == 'r') && (currCol-1>=0) && (maze[currRow][currCol-1] != ' ') ) next = true;
+	if ( !(currDir == 'l') && (currCol+1<nCols) && (maze[currRow][currCol+1] != ' ') ) next = true;
+	return next;
+    }
+    
     private boolean nextStep() {
 	boolean next = true;
-	//IO.print("curr = (" + currRow + ", " + currCol + ") with dir = " + currDir + " and nSteps = " + nSteps);
+	currRow = nextX();
+	currCol = nextY();
+	nSteps++;
 	
-	if ((nextX() < 0) || (nextX() >= nRows) || (nextY() < 0) || (nextY() >= nCols)) {
+	if (!nextAvailable()) {
 	    //we've reached the end!
 	    next = false;
 	} else {
 
-	    currRow = nextX();
-	    currCol = nextY();
-	    //nSteps++;
 	    char c = maze[currRow][currCol];
-
 	    if (c == '+') {
 		boolean dirChange = false;
 		//check neighbors for new direction
 		char c1 = ' ';
 		//check top neighbor
-		if ( !(currDir == 'd') && (currRow-1>=0) ) {
-		    if (maze[currRow-1][currCol] != ' ') {
-			c1 = maze[currRow-1][currCol];
-			currDir = 'u';
-			dirChange = true;
-		    }
+		if ( !(currDir == 'd') && (currRow-1>=0) && (maze[currRow-1][currCol] != ' ')) {
+		    c1 = maze[currRow-1][currCol];
+		    currDir = 'u';
+		    dirChange = true;
 		}
 		//check bottom neighbor
-		if ( !(currDir == 'u') && (currRow+1<nRows) && !dirChange ) {
-		    if (maze[currRow+1][currCol] != ' ') {
-			c1 = maze[currRow+1][currCol];
-			currDir = 'd';
-			dirChange = true;
-		    }
+		if ( !(currDir == 'u') && (currRow+1<nRows) && !dirChange &&  (maze[currRow+1][currCol] != ' ')) {
+		    c1 = maze[currRow+1][currCol];
+		    currDir = 'd';
+		    dirChange = true;
 		}
 		//check left neighbor
-		if ( !(currDir == 'r') && (currCol-1>=0) && !dirChange ) {
-		    if (maze[currRow][currCol-1] != ' ') {
-			c1 = maze[currRow][currCol-1];
-			currDir = 'l';
-			dirChange = true;
-		    }
+		if ( !(currDir == 'r') && (currCol-1>=0) && !dirChange &&  (maze[currRow][currCol-1] != ' ') ) {
+		    c1 = maze[currRow][currCol-1];
+		    currDir = 'l';
+		    dirChange = true;
 		}
 		//check right neighbor
-		if ( !(currDir == 'l') && (currCol+1<nCols) && !dirChange) {
-		    if (maze[currRow][currCol+1] != ' ') {
-			c1 = maze[currRow][currCol+1];
-			currDir = 'r';
-		    }
+		if ( !(currDir == 'l') && (currCol+1<nCols) && !dirChange && (maze[currRow][currCol+1] != ' ')) {
+		    c1 = maze[currRow][currCol+1];
+		    currDir = 'r';
 		}
-		//currRow = nextX();
-		//currCol = nextY();
-		//nSteps++;
-		if (c1 >= 'A' && c1 <='Z') {
-		    result += c1;
-		}
+
 		
 	    } else if (c >= 'A' && c<='Z') {
 		result += c;
 	    } //else | or - and then just continue
 
 	}
+
 	return next;
     }
 
     public void process() {
 	while (nextStep()) {
-	    nSteps++;
+	    //nSteps++;
 	}
 	IO.print("Part 1: " + result);
 	IO.print("Part 2: " + nSteps);
