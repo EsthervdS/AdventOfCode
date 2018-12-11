db82481e776284c6ab558b7002ecd1d7e104487d
Esther van der Stappen <esther.vanderstappen@gmail.com>
2018-12-11 17:42:00 +0100

    2015 Day 20 Part 1 & 2 optimized

--- 2015/day20/Houses.java
+++ 2015/day20/Houses.java
@@ -3,42 +3,56 @@ import java.util.*;
 
 public class Houses {
     long limit;
-    long[] gifts;
+    long[] gifts,gifts2;
     int n;
-    boolean found;
     
     public Houses(String input) {
 	limit = Long.parseLong(input);
-	n = (int) (limit/10);
+	n = (int) limit;
 	gifts = new long[n];
-	found = false;
+	gifts2 = new long[n];
 	for (int i=0; i<n; i++) {
 	    gifts[i] = 0;
+	    gifts2[i] = 0;
 	}
+	//Part 1
 	for (int house=1; house<n; house++) {
 	    for (int elve=1; elve<=Math.sqrt(house); elve++) {
 		if ((house % elve) == 0) {
-		    gifts[house] += (elve + house/elve)*10;
+		    if (elve==house/elve) {
+			gifts[house] += elve*10;
+		    } else {
+			gifts[house] += (elve + house/elve)*10;
+		    }
 		}
 		
 
 	    }
-	    int x = (int) Math.sqrt(house);
-	    if (Math.pow(x,2) == house) {
-		gifts[house] -= 10*Math.sqrt(house);
-	    }
 	    if (gifts[house] >= limit) {
 		IO.print("Part 1 : " + house);
-		found = true;
+		break;
+	    }
+	}
+
+	//Part 2
+	for (int elve=1; elve<=n; elve++) {
+	    for (int house=elve; (house<=elve*50 && house<n); house+=elve) {
+		if (elve==house/elve) {
+		    gifts2[house] += elve*11;
+		} else {
+		    gifts2[house] += (elve + house/elve)*11;
+		}
+	    }
+	}
+	for (int i=1; i<(n/10*50); i++) {
+	    if (gifts2[i]>=limit) {
+		IO.print("Part 2 : " + i);
 		break;
 	    }
 	}
     }
-    
     public static void main(String[] args) {
 	Houses hs = new Houses(args[0]);
-	//IO.print("Part 1: " + rp.uniques.size());
-	//IO.print("Part 2: " + rp.reduce());
     }
 
 }
